af78db97654cd2d73f739a03700924d3730ccc2a
Sasha <al.pryadchenko@gmail.com>
2022-05-21 21:51:43 +0200

    fixed server

--- app/server.js
+++ app/server.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 const app = require("../app");
 
 // const { DB_HOST, PORT = 3000 } = process.env;
+const PORT = process.env.PORT || 3000;
 const DB_HOST = process.env.MONGODB_URI;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    // app.listen(PORT);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
