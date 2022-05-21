0b9e4e71f29ac9c82134fac706880a2368c4ec21
Sasha <al.pryadchenko@gmail.com>
2022-05-21 20:29:51 +0200

    added patch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const path = require("path");
 
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
